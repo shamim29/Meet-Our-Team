bdfe6b28d5223a230f43df4eafe6f54ef541572d
Shahadat Hossain Shamim <shamim131666@gmail.com>
2021-08-04 12:40:26 +0600

    Characters-Selection
    Selecting a character from the Left Panel will show it on the Right Panel.

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] characters;
+
+
+
+    public void ChangeCharacter(int index)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characters[i].SetActive(false);
+        }
+
+        characters[index].SetActive(true);
+    }
+}
