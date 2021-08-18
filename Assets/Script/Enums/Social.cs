04d2b0881cbb09d57e1c85c9796d2480862e8da8
Shahadat Hossain Shamim <shamim131666@gmail.com>
2021-08-18 20:45:23 +0600

    MOT_V3

--- Assets/Script/Enums/Social.cs
+++ Assets/Script/Enums/Social.cs
@@ -1,4 +1,3 @@
-
 public enum SocialMedias
 {
      linkdin, artstation, instragram, github, facebook
--- Assets/Script/Enums/Technologies.cs
+++ Assets/Script/Enums/Technologies.cs
@@ -2,5 +2,5 @@ public enum Technologies
 {
     unity, dotnetcore, csharp, unreal, swift, blender, photoshop, sunstance,
     js, aws, docker, firebase, python, adobexd, android, flutter, jira, react,
-    nodeteam, wordpress
+    nodeteam, wordpress, three3ds, vive
 }
